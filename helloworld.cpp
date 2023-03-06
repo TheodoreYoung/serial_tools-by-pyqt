f0a19e8c7cbec1cb28bf3f00eae56f535bee4f12
TheodoreYoung <yangdonghe2008@126.com>
2023-03-07 01:23:31 +0800

    V1.2

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,11 +7,19 @@ using namespace std;
 int main(int argc, const char** argv) {
 
     vector<string> msg{"Hello", "C++", "World", "from", "VS Code"};
+    vector<string> name{"Yangdonghe","is a","hero"};
 
     for (const string &word : msg)
     {
         cout << word << " ";
     }
+    cout << endl;
+    for(const string & ydh : name)
+    {
+        cout<<ydh<<"+";
+    }
+    
+
 
     cout << endl;
     return 0;
